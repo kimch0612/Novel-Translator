4f23f3d1346facc5cbfaa224df1d1813067c6ada
Changhwan Kim <kimch061279@gmail.com>
2023-04-28 11:44:29 +0900

    크롤링 준비

--- crawling.py
+++ crawling.py
@@ -1,2 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time, os, urllib.request
+
+def init_chrome():
+    mobile_emulation = { "deviceName": "iPhone X" }
+    chrome_options = webdriver.ChromeOptions() 
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument(r'user-data-dir=C:\novelpia')
+    driver = webdriver.Chrome("c:/chromedriver.exe", options=chrome_options)
+    url = "https://novelpia.com/"
+    driver.implicitly_wait(7)
+    driver.get(url)
+    
 def crawler():
     pass
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from translater import translate
-from crawling import crawler
+from crawling import crawler, init_chrome
 
